82d7f4a3066f2e788a1ed4a9161e146644088a22
Eric Ianni <iannie@onid.oregonstate.edu>
2015-01-31 11:54:28 -0500

    Almost four hours and I have got the gists to load and I can seclect favorites and they will show up on page load,but I am about try to remove favorites and then filtering. Committing because this is a stable version.

--- Assignment3Part2/assignment3Part2.js
+++ Assignment3Part2/assignment3Part2.js
@@ -1,3 +1,7 @@
+window.onload = function() {
+    loadFavorites();
+};
+
 function loadPages()
 {
     var radios = document.getElementsByName('pages');
@@ -22,6 +26,7 @@ function loadPages()
     {
         if (pages.readyState === 4) {
             if (pages.status === 200) {
+                localStorage.setItem('pages', pages.responseText);
                 parsePages(pages.responseText);
             }
             else {
@@ -37,12 +42,13 @@ function parsePages(pages)
     var parsedJSON = JSON.parse(pages);
     var listings = [];
     var value;
+    var elem;
     for (var x = 0; x < parsedJSON.length; x++) {
         listings[x] = new listing(parsedJSON[x]);
+        elem = createListingElem(listings[x], 'listing');
+        document.getElementById('gistList').appendChild(elem);
     }
-    localStorage.setItem('listings', listings);
-    //console.log(typeof te[0]);
-    //console.log(test[2]['files'][0]['language']);
+    localStorage.setItem('listings', JSON.stringify(listings));
 }
 
 
@@ -62,6 +68,106 @@ function getValue(obj, name) {
 
 function listing(obj) {
     this.language = getValue(obj['files'], 'language');
-    this.description = obj['description'];
+    if (obj['description'] != '') {
+        this.description = obj['description'];
+    } else {
+        this.description = 'No Description';
+    }
     this.url = obj['html_url'];
+    this.id = obj['id'];
 };
+
+function createListingElem(obj, type) {
+    var listing = document.createElement('div');
+    listing.setAttribute('class', type);
+    listing.setAttribute('id', obj.id);
+
+    var description = document.createElement('div');
+    description.setAttribute('class', 'description');
+    var url = '<a href="' + obj.url + '">' + obj.description + '</a>';
+    description.innerHTML = url;
+    listing.appendChild(description);
+
+    var language = document.createElement('div');
+    language.setAttribute('class', 'language');
+    language.textContent = obj.language;
+    listing.appendChild(language);
+    
+    var fav_button = document.createElement('div');
+    fav_button.setAttribute('class', 'fav_button');
+    if (type === 'listing') {
+        fav_button.setAttribute('onclick', 'set_Favorite(this.parentNode)');
+    } else {
+        if (type === 'favorite') {
+                    fav_button.setAttribute('onclick', 'remove_Favorite(this.parentNode)');
+        } else {
+            alert('There was an error with the type of listing');
+        }
+    }
+    listing.appendChild(fav_button);
+
+    /*var webpage = document.createElement('div');
+    webpage.setAttribute('class', 'webpage');
+
+    var script = document.createElement('script')
+    script.setAttribute('class', 'portal');
+    script.setAttribute('src', obj.url + '.js');
+    webpage.appendChild(script);
+
+    listing.appendChild(webpage);*/
+
+    return listing;
+};
+
+function set_Favorite(listing) {
+    var elem = listing;
+    elem.setAttribute('class', 'favorites');
+    var gistList = document.getElementById('gistList');
+    gistList.removeChild(document.getElementById(listing.id));
+    var favorites = document.getElementById('favorites');
+    favorites.appendChild(elem);
+    var local = JSON.parse(localStorage.getItem('listings'));
+    var id = listing.getAttribute('id');
+    list_obj = new Object();
+    for (var prop in local) {
+        if (local[prop].id == id)
+        {
+            list_obj = local[prop];
+            console.log(typeof list_obj);
+        }
+
+    }
+    var fav_list = [];
+    if (typeof list_obj != 'object') {
+        alert('The favorite wasn\'t found in the parsed list');
+    } else {
+        if (localStorage.getItem('fav_list') === null) {
+            fav_list.push(list_obj);
+        } else {
+            fav_list = JSON.parse(localStorage.getItem('fav_list'));
+            fav_list.push(list_obj);
+        }
+    }
+    localStorage.setItem('fav_list', JSON.stringify(fav_list));
+    
+    //for()
+    //console.log(test);
+    //console.log(test[0]);
+};
+
+function loadFavorites() {
+    var fav_list = [];
+    fav_list = JSON.parse(localStorage.getItem('fav_list'));
+    var elem;
+    var favorites = document.getElementById('favorites');
+    for (var prop in fav_list) {
+        elem = createListingElem(fav_list[prop], 'favorite');
+        favorites.appendChild(elem);
+        console.log(prop);
+    }
+};
+
+function remove_Favorite(favorite) {
+
+};
+
