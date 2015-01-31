c303fcb0bdd218569a682d3aaaf57fb26eb2c8fd
Eric Ianni <iannie@onid.oregonstate.edu>
2015-01-31 18:02:33 -0500

    Been working since 8:30 this morning. I feel this commit is stable and feature complete. I am now going to try to spruce it up a tad.

--- Assignment3Part2/assignment3Part2.js
+++ Assignment3Part2/assignment3Part2.js
@@ -1,9 +1,15 @@
 window.onload = function() {
     loadFavorites();
+    var temp = [];
+    localStorage.setItem('pages', JSON.stringify(temp));
+    localStorage.setItem('listings', JSON.stringify(temp));
 };
+
+//fix the localstorage
 
 function loadPages()
 {
+    emptyElement('gistList', 'listing');
     var radios = document.getElementsByName('pages');
     var numOfPages;
     var url;
@@ -27,7 +33,7 @@ function loadPages()
         if (pages.readyState === 4) {
             if (pages.status === 200) {
                 localStorage.setItem('pages', pages.responseText);
-                parsePages(pages.responseText);
+                parseJSONElem(pages.responseText, 'listing');
             }
             else {
                 alert('There was a problem with the request');
@@ -36,19 +42,44 @@ function loadPages()
     };
 }
 
-function parsePages(pages)
+function parseJSONElem(JSONELEM, type, filter)
 {
-    //prompt(pages);
-    var parsedJSON = JSON.parse(pages);
+    var parsedJSON = JSON.parse(JSONELEM);
     var listings = [];
     var value;
     var elem;
     for (var x = 0; x < parsedJSON.length; x++) {
         listings[x] = new listing(parsedJSON[x]);
-        elem = createListingElem(listings[x], 'listing');
-        document.getElementById('gistList').appendChild(elem);
+        elem = createListingElem(listings[x], type);
+        if (!isFav(elem) && !isFiltered(elem, filter)) {
+           document.getElementById('gistList').appendChild(elem);
+        }
     }
     localStorage.setItem('listings', JSON.stringify(listings));
+}
+
+function isFiltered(elem, filter) {
+    if (typeof filter != 'undefined') {
+    filter = JSON.parse(filter);
+    var language = elem.getElementsByClassName('language')[0];
+    for (var prop in filter) {
+        if (language.textContent == filter[prop]) {
+            return true;
+        }
+    }
+}
+    return false;
+}
+
+function isFav(elem) {
+    fav_list = [];
+    fav_list = JSON.parse(localStorage.getItem('fav_list'));
+    for (var prop in fav_list) {
+        if (fav_list[prop].id == elem.id) {
+            return true;
+        }
+    }
+    return false;
 }
 
 
@@ -62,9 +93,7 @@ function getValue(obj, name) {
                 return String(obj[prop][data]);
         }
     }
-    
-
-};
+}
 
 function listing(obj) {
     this.language = getValue(obj['files'], 'language');
@@ -75,7 +104,7 @@ function listing(obj) {
     }
     this.url = obj['html_url'];
     this.id = obj['id'];
-};
+}
 
 function createListingElem(obj, type) {
     var listing = document.createElement('div');
@@ -96,10 +125,12 @@ function createListingElem(obj, type) {
     var fav_button = document.createElement('div');
     fav_button.setAttribute('class', 'fav_button');
     if (type === 'listing') {
+        fav_button.textContent = "Favorite";
         fav_button.setAttribute('onclick', 'set_Favorite(this.parentNode)');
     } else {
         if (type === 'favorite') {
                     fav_button.setAttribute('onclick', 'remove_Favorite(this.parentNode)');
+                    fav_button.textContent = "UnFavorite";
         } else {
             alert('There was an error with the type of listing');
         }
@@ -117,13 +148,15 @@ function createListingElem(obj, type) {
     listing.appendChild(webpage);*/
 
     return listing;
-};
+}
 
 function set_Favorite(listing) {
     var elem = listing;
-    elem.setAttribute('class', 'favorites');
+    elem.setAttribute('class', 'favorite');
     var gistList = document.getElementById('gistList');
     gistList.removeChild(document.getElementById(listing.id));
+    elem.getElementsByClassName('fav_button')[0].setAttribute('onclick', 'remove_Favorite(this.parentNode)');
+    elem.getElementsByClassName('fav_button')[0].textContent = "UnFavorite";
     var favorites = document.getElementById('favorites');
     favorites.appendChild(elem);
     var local = JSON.parse(localStorage.getItem('listings'));
@@ -133,7 +166,6 @@ function set_Favorite(listing) {
         if (local[prop].id == id)
         {
             list_obj = local[prop];
-            console.log(typeof list_obj);
         }
 
     }
@@ -153,7 +185,7 @@ function set_Favorite(listing) {
     //for()
     //console.log(test);
     //console.log(test[0]);
-};
+}
 
 function loadFavorites() {
     var fav_list = [];
@@ -163,11 +195,67 @@ function loadFavorites() {
     for (var prop in fav_list) {
         elem = createListingElem(fav_list[prop], 'favorite');
         favorites.appendChild(elem);
-        console.log(prop);
     }
-};
+}
 
 function remove_Favorite(favorite) {
+    var elem = favorite;
+    elem.setAttribute('class', 'listing');
+    elem.getElementsByClassName('fav_button')[0].setAttribute('onclick', 'set_Favorite(this.parentNode)');
+    var favorites = document.getElementById('favorites');
+    favorites.removeChild(document.getElementById(favorite.id));
+    var gistList = document.getElementById('gistList');
+    gistList.appendChild(elem);
+    var local = JSON.parse(localStorage.getItem('fav_list'));
+    var id = favorite.getAttribute('id');
+    list_obj = new Object();
+    temp = [];
 
-};
+    //console.log('full',local);
+    for (var prop in local) {
+        //console.log(local[prop].id);
+        if (local[prop].id != id)
+        {
+            list_obj = local[prop];
+            temp.push(list_obj);
+            //console.log('inside if localpropid');
+        }
 
+    }
+    //console.log(temp);
+    localStorage.setItem('fav_list', JSON.stringify(temp));
+    filter();
+}
+
+function filter() {
+    var checks = document.getElementsByName('checks');
+    var languages = [];
+    for (var x = 0; x < checks.length; x++)
+    {
+        if (checks[x].checked == true)
+        {
+            languages.push(checks[x].getAttribute('id'));
+        }
+    }
+    
+    emptyElement('gistList', 'listing');
+    var pages = localStorage.getItem('pages');
+    parseJSONElem(pages, 'listing', JSON.stringify(languages));
+}
+
+function emptyElement(elem, type) {
+    var temp = document.getElementById(elem);
+    var toRemove = document.getElementsByClassName(type);
+    var id;
+    while (toRemove.length) {
+        id = toRemove[0].id;
+        temp.removeChild(document.getElementById(id));
+        //alert(toRemove.length);
+    }
+
+}
+/*var elem = favorite;
+    elem.setAttribute('class', 'gistList');
+    elem.getElementsByClassName('fav_button')[0].setAttribute('onclick', 'set_Favorite(this.parentNode)');
+    var favorites = document.getElementById('favorites');
+    favorites.removeChild(document.getElementById(favorite.id));*/
